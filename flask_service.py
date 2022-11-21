dd94d83815a72d7e6e275975dfaa5f91e73628a5
Kamen Shah <kamenshah@gmail.com>
2022-11-21 15:38:44 -0800

    add working flask service

--- flask_service.py
+++ flask_service.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import os
 import time
 import json
@@ -19,72 +20,104 @@ def bad_request(message):
 
 
 """
-Route runs a model for PGA Success Prediction
+API runs a select model for PGA Success Prediction
 
-Route: http://localhost:8080/api/predict
+Route: http://localhost:8080/api/predictPGASuccess
+Request Type: POST
 
-JSON request object:
-{
-    "id" : _,
-    "data" :{
-        "Rounds": _,
-        "Fairway Percentage": _,
-        "Avg Distance": _,
-        "gir": _,
-        "Average Putts": _,
-        "Average Scrambling": _,
-        "Average Score": _,
-        "Points": _,
-        "Wins": _,
-        "Average SG Putts": _,
-        "Average SG Total": _,
-        "SG:OTT": _,
-        "SG:APR": _,
-        "SG:ARG": _
+JSON request format:
+    {
+        "id" : _,
+        "data" :{
+            "Average Score": _,
+            "Average SG Total": _,
+            "Points": _
+        "scaler": [MinMax, Standard, None]
+        "model": _,
     }
-    "scaler": [MinMax, Standard, None]
-    "model": _,
-}
+Example request:
+    {
+        "id": 1,
+        "data": {
+            "Average Score": 70,
+            "Average SG Total": 1.1,
+            "Points": 700
+        },
+        "scaler": "MinMax",
+        "model": "RidgeRegressor"
+    }
 
 returns: JSON object with id and prediction
-        - If error occurs, a details JSON error message
+        - If error occurs, a detailed JSON error message will be returned
 
 """
-@app.route('/api/predict',methods=['POST'])
-def predict():
-    if not request.json: 
+@app.route('/api/predictPGASuccess', methods=['POST'])
+def predictPGASuccess():
+
+    jsonObj = request.json
+
+    # Check if JSON is passed with correct keys
+    if not jsonObj: 
         return bad_request('Message must be JSON')
 
-    # Load model from pickle
-    # Apply data transformations
-    # Run predict 
-    # Return json 
-    req_id = str(request.json['id'])
+    if "id" not in jsonObj or "data" not in jsonObj or "scaler" not in jsonObj or "model" not in jsonObj:
+        return bad_request('Missing JSON key in request, please refer to documentation')
 
 
-    features = ["Rounds", "Fairway Percentage", "Avg Distance", "gir", "Average Putts", "Average Scrambling", "Average Score", "Points", "Wins", "Average SG Putts", "Average SG Total", "SG:OTT", "SG:APR", "SG:ARG"]
-    
-    
-    feats = request.json['data']
+    # Request ID
+    req_id = str(jsonObj['id'])
+
+    # Feature key validation 
+    features = ['Average Score', 'Average SG Total', 'Points']
+    # features = ["Rounds", "Fairway Percentage", "Avg Distance", "gir", "Average Putts", "Average Scrambling", "Average Score", "Points", "Wins", "Average SG Putts", "Average SG Total", "SG:OTT", "SG:APR", "SG:ARG"]
+
+    feats = jsonObj['data']
 
     for featureName in features:
         if featureName not in feats:
             return bad_request('Missing [' + featureName + '] from JSON data object')
 
+    for key in feats.keys():
+        feats[key] = [float(feats[key])]
 
-    scaler = str(request.json['scaler'])
-    if scaler != "MinMax" or scaler != "Standard" or scaler != "None":
+    # Scaler key validation
+    scaler = str(jsonObj['scaler'])
+    if scaler != "MinMax" and scaler != "Standard" and scaler != "None":
         return bad_request('Return valid scaler: MinMax, Standard, or None')
+    
+    # Load corresponding scaler and run fit on features
+    if scaler == "MinMax":
+        file = open('data/minMaxScaler.pkl', 'rb')
+        scaler = pickle.load(file)
+        file.close()
+    elif scaler == "Standard":
+        file = open('data/standardScaler.pkl', 'rb')
+        scaler = pickle.load(file)
+        file.close() 
+
+    feats = scaler.transform(pd.DataFrame(feats))
 
 
-    for file in os.listdir('models/'):
-        if file.endswith('.pkl'):
+    
+    # Model key validation
+    modelName = str(jsonObj['model'])
+    modelPath = 'models/' + modelName + '.pkl'
+    if os.path.exists(modelPath) == False:      
+        return bad_request('Not a valid model name, select from models folder: DecisionTree, LinearRegressor, NN, ect.')
+    
+    
+    # Load model and predict on data
+    file = open(modelPath, 'rb')
+    model = pickle.load(file)
+    file.close()
 
-    model = str(request.json['model'])
+    prediction = model.predict(feats)
+    
+    ret = {}
+    ret['id'] = req_id
+    ret['prediction'] = str(prediction[0])
 
-
-
-    return '{"id": "' + req_id + '"}'
+    return json.dumps(ret) 
 
 
 if __name__ == '__main__':
